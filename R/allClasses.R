f729baef1d71a5330dc0735a4febdaeb5f612ea0
jasenfinch <jsf9@aber.ac.uk>
2019-06-13 21:52:50 +0000

    added slots to classes

--- R/allClasses.R
+++ R/allClasses.R
@@ -1,8 +1,26 @@
 
-setClass('FunctionalEnrichment')
+setClass('EnrichmentParameters',
+         slots = list(
+           functional = 'list',
+           structural = 'list'
+         )
+)
+
+setClass('FunctionalEnrichment',
+         slots = list(
+           network = 'FELLA.DATA',
+           hits = 'tbl_df',
+           explanatory = 'character',
+           results = 'FELLA.USER'
+         )
+)
 
 setClass('StructuralEnrichment')
 
-setClass('Composition')
-
-setClass('Enrichment')
+setClass('Enrichment',
+         slots = list(
+           workflow = 'Workflow',
+           functional = 'FunctionalEnrichment',
+           structural = 'StructuralEnrichment'
+         )
+)

fe2c4e8de1f5490a9e571c4e9a00b4e174061364
jasenfinch <jsf9@aber.ac.uk>
2023-01-23 20:20:47 +0000

    replaced example data sets with a molecular formula assigned AnalysisData object

---
+++ R/assigned-data.R
@@ -0,0 +1,8 @@
+#' Example analysis data
+#' @format S4 object of class AnalysisData from the metabolyseR package.
+#' @source Example analysis data based on the Brachypodium distachyon 
+#' ecotype comparison FIE-MS metabolomic fingerprinting data set available 
+#' in the metaboData package. The features include putative molecular formula
+#' assignments.
+
+"assigned_data"

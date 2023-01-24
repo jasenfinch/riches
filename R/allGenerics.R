addd6da2ea9dc7baa6c4ab73910f07a61f1d5c71
jasenfinch <jsf9@aber.ac.uk>
2023-01-24 15:15:42 +0000

    remove allGenerics.R

--- R/allGenerics.R
+++
@@ -1,8 +0,0 @@
-
-setGeneric('organismCompounds',function(FELLA){
-  standardGeneric('organismCompounds')
-})
-
-setGeneric('plotGraph',function(x,comparison,type = 'diffusion'){
-  standardGeneric('plotGraph')
-})

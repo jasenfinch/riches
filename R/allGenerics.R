5406c066e180b447160b1ebb109fbd19b3979912
jasenfinch <jsf9@aber.ac.uk>
2023-01-24 10:00:26 +0000

    Remove enrichment parameters class and methods

--- R/allGenerics.R
+++ R/allGenerics.R
@@ -6,19 +6,3 @@ setGeneric('organismCompounds',function(FELLA){
 setGeneric('plotGraph',function(x,comparison,type = 'diffusion'){
   standardGeneric('plotGraph')
 })
-
-setGeneric('availablePairwises',function(x){
-  standardGeneric('availablePairwises')
-})
-
-#' @rdname EnrichmentParameters-accessors
-
-setGeneric('functional',function(x){
-  standardGeneric('functional')
-})
-
-#' @rdname EnrichmentParameters-accessors
-
-setGeneric('functional<-',function(x,value){
-  standardGeneric('functional<-')
-})

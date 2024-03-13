ac9b455756593e8616d47690430b2f5758e68ba2
jasenfinch <jsf9@aber.ac.uk>
2024-03-13 10:55:53 +0000

    check fix

--- R/riches.R
+++ R/riches.R
@@ -2,4 +2,4 @@
 globalVariables(c('.','feature','n_comparisons','intensity','name','Name','MF','comparison','log2_ratio',
                   'response','trend','nodes','com','NAME','level','classification','adjusted.p.value',
                   'kingdom','level','metric','value','n','Not explanatory & in class','Explanatory & in class',
-                  'Explanatory & not in class','p.value'))
+                  'Explanatory & not in class','p.value','Feature'))
